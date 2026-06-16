c4086d279135534a1a20f3da279cf8dbc89510f5
thais.fer <thais.fer_@outlook.com>
2026-06-16 16:40:39 -0300

    feat: define data structures and declare prototypes for new functions

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: thfernan <thfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 13:59:39 by thfernan          #+#    #+#             */
-/*   Updated: 2026/06/13 15:49:30 by thfernan         ###   ########.fr       */
+/*   Updated: 2026/06/16 15:54:59 by thfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@
 typedef struct s_philo
 {
 	int				index;
-	long			meal_count;
-	long			time_last_meal;
+	unsigned long	meal_count;
+	unsigned long	time_last_meal;
 	pthread_t		thread_id;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
@@ -34,10 +34,10 @@ typedef struct s_philo
 typedef struct s_sim
 {
 	int				number_philos;
-	long			time_to_die;
-	long			time_to_sleep;
-	long			time_to_eat;
-	long			start_time;
+	unsigned long	time_to_die;
+	unsigned long	time_to_eat;
+	unsigned long	time_to_sleep;
+	unsigned long	start_time;
 	int				must_eat; // -1 se não especificado
 	int				stop_sim; // flag de parada (0 = rodando, 1 = parar)
 	pthread_mutex_t	*forks;
@@ -45,5 +45,19 @@ typedef struct s_sim
 	pthread_mutex_t	*stop_lock;
 	t_philo			*philos;
 }	t_sim;
+
+// Main function
+long long	ft_atol(const char *str);
+int			parse_args(int argc, char **argv, t_sim *sim);
+//int		main(int argc, char **argv);
+
+// Init simulation
+int			init_sim(t_sim *sim, int i);
+
+// Free and destroy
+void		destroy_meal_locks(t_sim *sim, int count);
+void		destroy_forks(t_sim *sim);
+void		cleanup_sim(t_sim *sim, int philos_initialized);
+int			clear_philos_and_forks(t_sim *sim);
 
 #endif
